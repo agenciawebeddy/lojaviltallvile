ebcda575a0ee502c8d51276e5f28ef92d3e9e0d6
[dyad] <edson@webeddy.com.br>
2025-11-29 14:29:54 -0300

    [dyad] Fixed new user registration database error - wrote 3 file(s), executed 1 SQL queries

--- supabase/functions/send-admin-notification/index.ts
+++ supabase/functions/send-admin-notification/index.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { serve } from "https://deno.land/std@0.190.0/http/server.ts";
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 import { Resend } from 'https://esm.sh/resend@1.1.0';
